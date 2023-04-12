af64dbb984509f49a155681cec2314635a670617
Dennohkamash <dennohkamash134@gmail.com>
2023-04-12 12:23:23 +0300

    time_finder App

--- lib/Secondscreen.dart
+++ lib/Secondscreen.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:country_list_pick/country_list_pick.dart';
 import 'package:country_flags/country_flags.dart';
+import 'package:intl/intl.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/standalone.dart';
+import 'package:time/time.dart';
 
 class SecondScreen extends StatelessWidget {
-  const SecondScreen({super.key});
+  const SecondScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +16,7 @@ class SecondScreen extends StatelessWidget {
 }
 
 class Rootwidget extends StatefulWidget {
-  const Rootwidget({super.key});
+  const Rootwidget({Key? key}) : super(key: key);
 
   @override
   State<Rootwidget> createState() => _RootwidgetState();
@@ -20,6 +24,11 @@ class Rootwidget extends StatefulWidget {
 
 class _RootwidgetState extends State<Rootwidget> {
   String _selectedCountryCode = "";
+  late String _selectedCountryTime;
+  Future<Location> getTimeZoneLocation(String timeZoneId) async {
+    final database = await timeZoneDatabase;
+    return database.locations[timeZoneId]!;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -45,9 +54,16 @@ class _RootwidgetState extends State<Rootwidget> {
             showEnglishName: true,
           ),
           initialSelection: '+1',
-          onChanged: (CountryCode? code) {
+          onChanged: (CountryCode? code) async {
+            final now = DateTime.now();
+            final location = await getTimeZoneLocation(code!.code!);
+            final timeZone = location.timeZone;
+            final currentTime =
+                tz.TZDateTime.now(timeZone as tz.Location).toLocal();
+            final formattedTime = DateFormat.jm().format(currentTime);
             setState(() {
-              _selectedCountryCode = code!.code!;
+              _selectedCountryCode = code.code!;
+              _selectedCountryTime = formattedTime;
             });
           },
         ),
